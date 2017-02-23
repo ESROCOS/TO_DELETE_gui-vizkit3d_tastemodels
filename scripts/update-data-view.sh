3a626a24ae598a65f9d04c939274d9995319d7da
Miguel Munoz <mmunoz@gmv.com>
2017-02-23 12:48:19 +0100

    Import .asn files from install directory

--- scripts/update-data-view.sh
+++ scripts/update-data-view.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 export FILES="
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Angle.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/BodyState.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Covariance.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/DepthMap.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Eigen.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Joints.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/JointState.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/LaserScan.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Motion2D.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Point.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Pointcloud.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Pose.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/RigidBodyState.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/SonarBeam.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Time.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/TransformWithCovariance.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/TwistWithCovariance.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/Waypoint.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/taste-types.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/base/taste-extended.asn \
-$AUTOPROJ_CURRENT_ROOT/base/taste/asn1_types/asn/sargon/vizkit3d/Vizkit3D.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Angle.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/BodyState.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Covariance.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/DepthMap.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Eigen.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Joints.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/JointState.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/LaserScan.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Motion2D.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Point.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Pointcloud.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Pose.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/RigidBodyState.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/SonarBeam.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Time.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/TransformWithCovariance.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/TwistWithCovariance.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/Waypoint.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/taste-types.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/base/taste-extended.asn \
+$AUTOPROJ_CURRENT_ROOT/install/share/taste/asn1_types/sargon/vizkit3d/Vizkit3D.asn \
 "
 
 echo "Updating DataView.aadl"
